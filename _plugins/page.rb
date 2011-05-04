954d9fc75ab1e3f31493946efd18a2b0b2d47248
Ernie Bello <ernie.bello@gmail.com>
2011-05-04 12:13:29 -0400

    added subnavigation support based on folders; added examples of primary nav and sub nav to default template

--- _plugins/page.rb
+++ _plugins/page.rb
@@ -1,5 +1,31 @@
 module Jekyll
   class Page
     attr_accessor :content_blocks
+    
+    def subfolder
+      @dir
+    end
+    
+    def hierarchy_array
+      subfolder.split('/')
+    end
+    
+    def hierarchy_at(levels_up)
+      arr = hierarchy_array
+      if (arr.size - levels_up > -1)
+        arr = arr.first(arr.size - levels_up)
+      end
+      arr.join('/')
+    end
+    
+    alias orig_to_liquid to_liquid
+    def to_liquid
+      # we want index pages to be included with the navigation one level above, but only if it's not on the first level
+      orig_to_liquid.deep_merge({
+        "subfolder" => (index? && hierarchy_array.size > 2) ? hierarchy_at(1) : subfolder,
+        "hierarchy_array" => hierarchy_array,
+        "parent" => hierarchy_at(1)
+      })
+    end
   end
 end
--- _plugins/sorted_nav.rb
+++ _plugins/sorted_nav.rb
@@ -5,7 +5,13 @@ module Jekyll
     priority :high
 
     def generate(site)
-      site.config['sorted_navigation'] = site.pages.select{|p| !p.to_liquid["navorder"].nil?}.sort_by! { |p| p.to_liquid["navorder"] }
+      site.config['primary_navigation'] = site.pages.select{|p| !p.to_liquid["primarynavorder"].nil?}.sort_by! { |p| p.to_liquid["primarynavorder"] }
+      
+      sorted_nav = Hash.new { |hash, key| hash[key] = [] }
+      site.pages.select{|p| !p.to_liquid["navorder"].nil?}.sort_by! { |p| p.to_liquid["navorder"] }.each do |p|
+        sorted_nav[p.to_liquid["subfolder"]] << p
+      end
+      site.config["sorted_navigation"] = sorted_nav
     end
 
   end
