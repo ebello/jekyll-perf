dd2db712aaffd08764869c9a7d6fffcca0cc4015
Ernie Bello <ernie.bello@gmail.com>
2011-05-25 18:34:11 -0400

    added date sorting for pages, will sort by date descending

--- _plugins/sorted_nav.rb
+++ _plugins/sorted_nav.rb
@@ -8,6 +8,12 @@ module Jekyll
       site.config['primary_navigation'] = site.pages.select{|p| !p.to_liquid["primarynavorder"].nil?}.sort_by! { |p| p.to_liquid["primarynavorder"] }
       
       sorted_nav = Hash.new { |hash, key| hash[key] = [] }
+      # sort pages that have date specified, order by date descending
+      site.pages.select{|p| !p.to_liquid["date"].nil? && p.to_liquid["navorder"].nil?}.sort! { |a, b| b.to_liquid["date"] <=> a.to_liquid["date"] }.each do |p|
+        sorted_nav[p.to_liquid["subfolder"]] << p
+      end
+      
+      # sort pages that have navorder specified, takes precedence over date
       site.pages.select{|p| !p.to_liquid["navorder"].nil?}.sort_by! { |p| p.to_liquid["navorder"] }.each do |p|
         sorted_nav[p.to_liquid["subfolder"]] << p
       end
