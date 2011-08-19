bd6bcc4b0b916c1c653ae417b77c93afb6ff1de7
Ernie Bello <ernie.bello@gmail.com>
2011-08-19 13:39:17 -0400

    added url liquid tag in order to specify url's by page id

---
+++ _plugins/page_url_tag.rb
@@ -0,0 +1,26 @@
+module Jekyll
+  class PageUrlTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      # for some reason liquid escapes quotes for strings
+      @pageid = text.strip.gsub(/\"/, '')
+    end
+
+    def render(context)
+      url = ""
+      context.registers[:site].pages.each do |p|
+        if @pageid == p.to_liquid["id"]
+          url = p.url
+          break
+        end
+      end
+      if url.empty?
+        puts "ERROR - could not find url for page id: #{@pageid}"
+      end
+      return url
+    end
+  end
+end
+
+Liquid::Template.register_tag('url', Jekyll::PageUrlTag)
