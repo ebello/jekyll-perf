531b47a4f3ecb0eae363e0798b423ce90db61f13
Ernie Bello <bello@brandlogic.com>
2012-10-15 17:34:14 -0400

    added option to disable HTML generation for JSON pages

--- _plugins/generate_json.rb
+++ _plugins/generate_json.rb
@@ -14,21 +14,23 @@ module Jekyll
 
     def generate(site)
       site.pages.select{|p| p.ext == ".json"}.each do |p|
-        
-        # load json into property so we can access with liquid later
-        p.json = JSON.parse(p.content)
-        
-        # write out json file
-        dir = File.join(site.dest, p.to_liquid["subfolder"])
-        FileUtils.mkdir_p(dir)
-        
-        File.open(File.join(dir, p.name), 'w') do |f|
-          f.write(p.content)
-          f.close
+
+        unless p.to_liquid["makehtml"] == false
+          # load json into property so we can access with liquid later
+          p.json = JSON.parse(p.content)
+          
+          # write out json file
+          dir = File.join(site.dest, p.to_liquid["subfolder"])
+          FileUtils.mkdir_p(dir)
+          
+          File.open(File.join(dir, p.name), 'w') do |f|
+            f.write(p.content)
+            f.close
+          end
+          
+          # add to static files so it doesn't delete file in cleanup
+          site.static_files << JsonFile.new(site, site.dest, p.to_liquid["subfolder"], p.name)
         end
-        
-        # add to static files so it doesn't delete file in cleanup
-        site.static_files << JsonFile.new(site, site.dest, p.to_liquid["subfolder"], p.name)
       end
       
     end
