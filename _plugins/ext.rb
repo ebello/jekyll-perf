7b3a8dfea2c1444303027ef9fa57109fd1599851
Ernie Bello <ernie@ern.me>
2013-06-27 01:55:06 -0400

    added option to preserve licensing for javascripts as well as not compile a script if .min is in the name

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,2 +1,23 @@
 require "jekyll-assets"
 require "jekyll-assets/compass"
+
+# following is a workaround for keeping copyrights intact
+# https://github.com/ixti/jekyll-assets/issues/34
+require "sprockets/uglifier_compressor"
+
+class Sprockets::UglifierCompressor
+  def evaluate(context, locals, &block)
+    if context.pathname.to_s =~ /\.min\./
+      data
+    else
+      # Feature detect Uglifier 2.0 option support
+      if Uglifier::DEFAULTS[:copyright]
+        # Uglifier < 2.x
+        Uglifier.new(:copyright => true).compile(data)
+      else
+        # Uglifier >= 2.x
+        Uglifier.new(:comments => :copyright).compile(data)
+      end
+    end
+  end
+end
