4ee25b5451f6ff63212942ce57c7eb6e1daf5574
Ernie Bello <bello@brandlogic.com>
2013-10-02 12:51:20 -0400

    added ability to pull down external source of JSON in order to build pages

--- _plugins/generate_json.rb
+++ _plugins/generate_json.rb
@@ -23,10 +23,18 @@ module Jekyll
       site.allpages.select{|p| p.ext == ".json"}.each do |p|
 
         unless p.to_liquid["makehtml"] == false
+          content = p.content
+
+          # if an external source exists for the JSON file, pull it down and use that instead
+          if p.to_liquid["source"]
+            require 'open-uri'
+            content = open(p.to_liquid["source"]) {|io| io.read}
+          end
+
           # set json property to content. it will be converted to actual JSON in do_layout.
-          p.json = p.content
+          p.json = content
           
-          site.pages << JsonFile.new(site, site.source, p.to_liquid["subfolder"], p.name, p.content)
+          site.pages << JsonFile.new(site, site.source, p.to_liquid["subfolder"], p.name, content)
         end
       end
       
